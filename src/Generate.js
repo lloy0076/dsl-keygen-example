df736b234bfd850b1618006e7c60b5cb6f3b2050
lloy0076 <lloy0076@adam.com.au>
2020-02-26 10:41:19 -0500

    Generates keys.
    For some reason, though, the re-import is not working.

--- src/Generate.js
+++ src/Generate.js
@@ -5,17 +5,80 @@ import {
 } from 'reactstrap';
 
 export default function Generate() {
-    const privateKey = localStorage.getItem('private_key') || '';
-    const publicKey = localStorage.getItem('public_key') || '';
+    let initialPrivateKey = '';
+    let initialPublicKey = '';
 
-    const [formValue, setFormValue] = useState({ privateKey, publicKey });
+    const storedPrivateKey = localStorage.getItem('private_key');
+
+    if (storedPrivateKey) {
+        try {
+            const parsed = JSON.parse(storedPrivateKey);
+            initialPrivateKey = parsed.key;
+        } catch (error) {
+            console.error('Error parsing private key', error);
+        }
+    }
+
+    const storedPublicKey = localStorage.getItem('public_key');
+
+    if (storedPublicKey) {
+        try {
+            const parsed = JSON.parse(storedPublicKey);
+            initialPublicKey = parsed.key;
+        } catch (error) {
+            console.error('Error parsing public key', error);
+        }
+    }
+
+    let [privateKey, setPrivateKey] = useState(initialPrivateKey);
+    let [publicKey, setPublicKey] = useState(initialPublicKey);
 
     function generateKeyPair() {
         const { crypto } = window;
 
+        function a2str(buf) {
+            return String.fromCharCode.apply(null, new Uint8Array(buf));
+        }
+
+        function str2ab(str) {
+            const buf = new ArrayBuffer(str.length);
+            const bufView = new Uint8Array(buf);
+            for (let i = 0, strLen = str.length; i < strLen; i++) {
+                bufView[i] = str.charCodeAt(i);
+            }
+            return buf;
+        }
+
+        function importPrivateKey(pem) {
+            // fetch the part of the PEM string between header and footer
+            const pemHeader = '-----BEGIN PRIVATE KEY-----';
+            const pemFooter = '-----END PRIVATE KEY-----';
+            const pemContents = pem.substring(pemHeader.length, pem.length - pemFooter.length);
+            // base64 decode the string to get the binary data
+            const binaryDerString = atob(pemContents);
+            // convert from a binary string to an ArrayBuffer
+            const binaryDer = str2ab(binaryDerString);
+
+            crypto.subtle.digest('SHA-256', binaryDer).then((result) => console.log(btoa(result)));
+
+            return crypto.subtle.importKey(
+                'pkcs8',
+                binaryDer,
+                {
+                    name: 'RSASSA-PKCS1-V1_5',
+                    modulusLength: 4096,
+                    // i.e. 65537
+                    publicExponent: new Uint8Array([1, 0, 1]),
+                    hash: 'SHA-256',
+                },
+                true,
+                ['encrypt', 'decrypt', 'verify', 'sign'],
+            );
+        }
+
         crypto.subtle.generateKey(
             {
-                name: 'RSA-OAEP',
+                name: 'RSASSA-PKCS1-V1_5',
                 modulusLength: 4096,
                 // i.e. 65537
                 publicExponent: new Uint8Array([1, 0, 1]),
@@ -24,7 +87,43 @@ export default function Generate() {
             true,
             ['encrypt', 'decrypt', 'verify', 'sign'],
         ).then((keyPair) => {
-            console.log(keyPair.privateKey);
+            crypto.subtle.exportKey('pkcs8', keyPair.privateKey).then(
+                (key) => {
+                    crypto.subtle.digest('SHA-256', key).then((result) => console.log(btoa(result)));
+
+                    const exportedAsString = a2str(key);
+                    const exportedAsBase64 = btoa(exportedAsString);
+
+                    const exported = {
+                        key: `-----BEGIN PRIVATE KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`,
+                    };
+
+                    localStorage.setItem('private_key', JSON.stringify(exported));
+
+                    setPrivateKey(exported.key);
+
+                    console.log('Attempting to import key', exported);
+
+                    importPrivateKey(exported.key).then((result) => {
+                        console.log('Import went OK.');
+                    }).catch((error) => console.trace(error));
+                },
+            ).catch((error) => console.error(error));
+
+            crypto.subtle.exportKey('spki', keyPair.publicKey).then(
+                (key) => {
+                    const exportedAsString = a2str(key);
+
+                    const exportedAsBase64 = btoa(exportedAsString);
+                    const exported = {
+                        key: `-----BEGIN PUBLIC KEY-----\n${exportedAsBase64}\n-----END PRIVATE KEY-----`,
+                    };
+
+                    localStorage.setItem('public_key', JSON.stringify(exported));
+
+                    setPublicKey(exported.key);
+                },
+            ).catch((error) => console.error(error));
         });
     }
 
@@ -34,9 +133,9 @@ export default function Generate() {
 
             <Card>
                 <CardBody>
-                    <CardTitle><span style={{ fontWeight: 'bolder' }}>Public Key</span></CardTitle>
+                    <CardTitle><span style={{ fontWeight: 'bolder' }}>Private Key</span></CardTitle>
                     <pre>
-                        {formValue.publicKey}
+                        {privateKey}
                     </pre>
                 </CardBody>
             </Card>
@@ -45,9 +144,9 @@ export default function Generate() {
 
             <Card>
                 <CardBody>
-                    <CardTitle><span style={{ fontWeight: 'bolder' }}>Private Key</span></CardTitle>
+                    <CardTitle><span style={{ fontWeight: 'bolder' }}>Public Key</span></CardTitle>
                     <pre>
-                        {formValue.privateKey}
+                        {publicKey}
                     </pre>
                 </CardBody>
             </Card>
